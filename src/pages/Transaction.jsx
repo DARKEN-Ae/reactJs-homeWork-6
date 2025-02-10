f502284fcfaff96651c4684a92fd90e76f8d2645
darken <otashblogs@gmail.com>
2025-02-10 18:34:42 +0500

    asd

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -6,28 +6,22 @@ const Transaction = () => {
     {
       id: 1,
       name: "Alisher",
-      type: "deposit",
       amount: 50.0,
       currency: "USDT",
-      status: "completed",
       date: "2024-02-10T12:30:00Z",
     },
     {
       id: 2,
       name: "Javohir",
-      type: "withdrawal",
       amount: 20.5,
       currency: "USDT",
-      status: "pending",
       date: "2024-02-09T15:45:00Z",
     },
     {
       id: 3,
       name: "Shoxobiddin",
-      type: "deposit",
       amount: 100.0,
       currency: "USDT",
-      status: "failed",
       date: "2024-02-08T09:20:00Z",
     },
   ]);
@@ -39,21 +33,19 @@ const Transaction = () => {
         </div>
         <div className="transaction-cards">
           <div className="transaction-card">
-            {/* <ul>
+            <ul>
               {transaction.map((e) => (
                 <li key={e.id}>
-                  <p>{e.type.toUpperCase()}</p> - {e.amount}
-                  {e.currency} <span>({e.status})</span>
+                  <p>Ismi: {e.name}</p>
+                  <p>Amount: {e.type}</p>
+                  <p>{e.type}</p>
+                  <p>{e.type}</p>
+                  <p>{e.type}</p>
+                  <p>{e.type}</p>
+                  <p>{e.type}</p>
                 </li>
               ))}
-            </ul> */}
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+            </ul>
           </div>
         </div>
       </div>
